be252f8a71a8b524d9d72caf6289e4b847517fb0
nuronial_block <ahnaf@furqansoftware.com>
2016-01-25 23:49:39 +0600

    Implement AccountPasswird struct

---
+++ data/accountpassword.go
@@ -0,0 +1,9 @@
+package data
+
+type AccountPassword struct {
+	Salt       []byte `bson:"salt"`
+	Iteration  int    `bson:"iteration"`
+	KeyLength  int    `bson:"key_length"`
+	DerivedKey []byte `bson:"derived_key"`
+	Algorithm  string `bson:"algorithm"`
+}
