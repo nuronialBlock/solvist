5b37b9e715945bae94178aca68e50c7fb1558c37
nuronial_block <ahnaf@furqansoftware.com>
2016-01-25 23:20:10 +0600

    Implement Account struct

---
+++ data/account.go
@@ -0,0 +1,21 @@
+// Copyright @nuronial_block
+package data
+
+import (
+	"time"
+
+	"labix.org/v2/mgo/bson"
+)
+
+type Account struct {
+	ID     bson.ObjectId `bson:"_id"`
+	Name   string        `bson:"name"`
+	Handle string        `bson:"handle"`
+
+	Password AccountPassword{} `bson:"password"`
+	Emails []AccountEmail{} `bson:"emails"`
+	Banned bool `bson:"banned"`
+
+	CreatedAt  time.Time `bson:"created_at"`
+	ModifiedAt time.Time `bson:"modified_at"`
+}
