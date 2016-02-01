cd28a66b051abfb31e5bfe71f32b2e6cd4dfb29d
nuronial_block <ahnaf@furqansoftware.com>
2016-02-01 23:41:06 +0600

    Implements Put menthod in Task data struct

--- data/task.go
+++ data/task.go
@@ -1,7 +1,11 @@
 // Copyright @nuronial_block
 package data
 
-import "labix.org/v2/mgo/bson"
+import (
+	"time"
+
+	"labix.org/v2/mgo/bson"
+)
 
 type Task struct {
 	ID          bson.ObjectId `bson:"_id"`
@@ -9,5 +13,19 @@ type Task struct {
 	ProblemOj   string        `bson:"problem_oj"`
 	ProblemID   string        `bson:"problem_id"`
 
+	ModifiedAt time.Time `bson:"modified_at"`
+	CreatedAt  time.Time `bson:"created_at"`
+
 	Solved bool `bson:"solved"`
+}
+
+func (t *Task) Put() error {
+	t.ModifiedAt = time.Now()
+	if t.ID == "" {
+		t.CreatedAt = t.ModifiedAt
+		t.ID = bson.NewObjectId()
+	}
+
+	_, err := sess.DB("").C(taskC).UpsertId(t.ID, t)
+	return err
 }
