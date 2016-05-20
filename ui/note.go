ad8b9234a8578fa740b26de44ef19aba40f223ed
nuronial_block <ahnaf@furqansoftware.com>
2016-05-20 20:53:47 +0600

    Implement HandleNoteCreate handlerfunc; closes #26

--- ui/note.go
+++ ui/note.go
@@ -2,7 +2,15 @@
 
 package ui
 
-import "net/http"
+import (
+	"bytes"
+	"net/http"
+
+	"github.com/gorilla/schema"
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/nuronialBlock/solvist/solvist/data"
+	"github.com/russross/blackfriday"
+)
 
 // ServeNoteNewForm serves a new note form.
 func ServeNoteNewForm(w http.ResponseWriter, r *http.Request) {
@@ -12,9 +20,58 @@ func ServeNoteNewForm(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// NoteValues stores the values of the note.
+type NoteValues struct {
+	ProblemName string `schema:"name"`
+	ProblemOJ   string `schema:"oj"`
+	ProblemID   string `schema:"id"`
+	TopicName   string `schema:"topic"`
+	Catagory    string `schema:"catagory"`
+	Text        string `schema:"text"`
+}
+
+// HandleNoteCreate handles to create new note.
+func HandleNoteCreate(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		ServeInternalServerError(w, r)
+	}
+
+	formValues := NoteValues{}
+	decoder := schema.NewDecoder()
+	err = decoder.Decode(&formValues, r.PostForm)
+	if err != nil {
+		ServeInternalServerError(w, r)
+	}
+
+	note := data.Note{}
+	note.ProblemName = formValues.ProblemName
+	note.ProblemOJ = formValues.ProblemOJ
+	note.ProblemID = formValues.ProblemID
+	note.TopicName = formValues.TopicName
+	note.Catagory = formValues.Catagory
+
+	textBytes := bytes.NewBufferString(formValues.Text).Bytes()
+	unsafe := blackfriday.MarkdownCommon(textBytes)
+	htmlBuf := bluemonday.UGCPolicy().SanitizeBytes(unsafe)
+	html := bytes.NewBuffer(htmlBuf).String()
+	note.Text = html
+
+	err = note.Put()
+	if err != nil {
+		ServeInternalServerError(w, r)
+	}
+
+	http.Redirect(w, r, "/notes", http.StatusSeeOther)
+}
+
 func init() {
 	Router.NewRoute().
 		Methods("Get").
 		Path("/notes/new").
 		HandlerFunc(ServeNoteNewForm)
+	Router.NewRoute().
+		Methods("Post").
+		Path("/notes/new").
+		HandlerFunc(HandleNoteCreate)
 }
