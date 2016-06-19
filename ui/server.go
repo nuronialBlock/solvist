ecb5656b55d0ccea44413e709e3f133535c576bd
nuronial_block <ahnaf@furqansoftware.com>
2016-06-19 23:42:54 +0600

    Implement login mechanism

--- ui/server.go
+++ ui/server.go
@@ -6,10 +6,18 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/securecookie"
+	"github.com/gorilla/sessions"
+)
+
+const (
+	cookieLength = 20
 )
 
 // Router registers routes to be matched and dispatches a handler.
 var Router = mux.NewRouter()
+
+var store = sessions.NewCookieStore(securecookie.GenerateRandomKey(cookieLength))
 
 // Server stores router.
 type Server struct {
@@ -18,6 +26,10 @@ type Server struct {
 
 // ServeHTTP calls the HandlerFunc based on request r.
 func (s Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	sess, err := store.Get(r, "very-secret")
+	if err != nil {
+		ServeInternalServerError(w, r)
+	}
 	s.router.ServeHTTP(w, r)
 }
 
