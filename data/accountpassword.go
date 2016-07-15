0a06f29e59969ac8a1204f4fd62628e590bc2af5
nuronial_block <ahnaf@furqansoftware.com>
2016-07-15 17:38:44 +0600

    Implement NewAccountPassword function; closes #66

--- data/accountpassword.go
+++ data/accountpassword.go
@@ -2,6 +2,13 @@
 
 package data
 
+import (
+	"crypto/rand"
+	"crypto/sha1"
+
+	"golang.org/x/crypto/pbkdf2"
+)
+
 // AccountPassword stores the password informations.
 type AccountPassword struct {
 	Salt       []byte `bson:"salt"`
@@ -9,4 +16,20 @@ type AccountPassword struct {
 	KeyLength  int    `bson:"key_length"`
 	DerivedKey []byte `bson:"derived_key"`
 	Algorithm  string `bson:"algorithm"`
+}
+
+// NewAccountPassword generates an encrypted password.
+func NewAccountPassword(clear string) (AccountPassword, error) {
+	accPass := AccountPassword{}
+	accPass.Salt = make([]byte, 32)
+	_, err := rand.Read(accPass.Salt)
+	if err != nil {
+		return AccountPassword{}, err
+	}
+
+	accPass.Algorithm = "SHA1"
+	accPass.Iteration = 4096
+	accPass.KeyLength = 32
+	accPass.DerivedKey = pbkdf2.Key(clear, accPass.Salt, accPass.Iteration, accPass.KeyLength, sha1.New)
+	return accPass, nil
 }
